df996b3ab2cc801e965d6362ae2e7ea968267447
Irina1707 <irina_niv@mail.ru>
2022-03-20 17:11:18 +0300

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+
+
+ class App extends React.Component {
+  state = {
+  contacts: [],
+  name: ''
+}
 };
+
+export default App;
